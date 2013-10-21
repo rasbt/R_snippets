31f22194396b72cee7defa310b460333d9d36c26
Sebastian Raschka <raskberry@mail.com>
2013-10-21 12:53:54 -0400

    Create h1.R

---
+++ heatmaps/h1.R
@@ -0,0 +1,43 @@
+library(gplots)
+library(RColorBrewer)
+
+#########################################################
+### reading in data and transform it to matrix format
+#########################################################
+
+data <- read.csv("h1_dataset.csv", comment.char="#")
+rnames <- data[,1]				# assign labels in column 1 to "rnames"
+mat_data <- data.matrix(data[,2:ncol(data)])	# transform column 2-5 into a matrix
+rownames(mat_data) <- rnames			# assign row names 
+
+
+
+#########################################################
+### customizing and plotting heatmap
+#########################################################
+
+# creates a own color palette from red to green
+my_palette <- colorRampPalette(c("red", "yellow", "green"))(n = 299)
+
+# (optional) defines the color breaks manually for a "skewed" color transition
+col_breaks = c(seq(-1,0,length=100),seq(0,0.7,length=100),seq(0.7,1,length=100))
+
+# creates a 5 x 5 inch image
+png("h1.png",	
+  width = 5*300,    # 5 x 300 pixels
+  height = 5*300,
+  res = 300,        # 300 pixels per inch
+  pointsize = 8)    # smaller font size
+
+heatmap.2(mat_data, 
+  cellnote = mat_data,  # same data set for cell labels
+  notecol="black",      # change font color of cell labels to black
+  density.info="none", 	# turns off density plot inside color legend
+  trace="none", 	# turns off trace lines inside the heat map
+  margins =c(12,9), 	# widens margins around plot
+  col=my_palette,       # use on color palette defined earlier 
+  breaks=col_breaks,	# enable color transition at specified limits
+  dendrogram="row",     # only draw a row dendrogram
+  Colv="NA")            # turn off column clustering
+
+dev.off()
